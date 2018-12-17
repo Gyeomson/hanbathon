e3254e6c987d14559eaa1213551fad6bd3bdfc3f
hanyoon <mit2011@likelion.org>
2018-12-17 07:54:24 +0000

    deploy 181217

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ActiveRecord::Base
     validates :email, uniqueness: { message: "이미 존재합니다."}
     VALID_MOBILE_NUMBER = /\A010\-([1-9]{1})([0-9]{3})\-([0-9]{4})\z/
     validates :mobile, format: {with: VALID_MOBILE_NUMBER ,message: "형식에 맞지 않습니다."}, uniqueness: {  message: "이미 존재합니다."}
-    VALID_ID_NUMBER = /\A201([0-9]{5})\z/
-    validates :id_number, format: {with: VALID_ID_NUMBER, message: "형식에 맞지 않습니다."}, uniqueness: {  message: "이미 존재합니다."}
+    # VALID_ID_NUMBER = /\A201([0-9]{5})\z/ , format: {with: VALID_ID_NUMBER, message: "형식에 맞지 않습니다."}
+    validates :id_number, uniqueness: {  message: "이미 존재합니다."}
 end
